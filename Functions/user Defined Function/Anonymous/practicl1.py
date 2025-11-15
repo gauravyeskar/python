8c6db11662666ac75b208092b5d06ecc54d954ed
gaurav <gauravyeskar@gmail.com>
2025-11-15 14:58:24 +0530

    Functions file with types

--- Functions/user Defined Function/Anonymous/practicl1.py
+++ Functions/user Defined Function/Anonymous/practicl1.py
@@ -11,17 +11,19 @@ Syn:-  varnm = lambda params_list:expression
 '''
 r = lambda x:x+10
 print(r(100))
+print("-"*20)
 
 hd = lambda a,b:a+b
 print(hd(10,20))
+print("-"*20)
 
 findmax = lambda a,b :a if a > b else b
 print(findmax(20,10))
+print("-"*20)
 
 getword = lambda line : line.split()
 line = input("Enter the string: ")
 words = getword(line)
 for word in words:
     print("\t {}".format(word))
-
-
+print("-"*20)
